d550de8232ea79871f9cffd100cee808dc1485f5
paiyar <pavan.aiyar@sony.com>
2022-03-07 19:31:20 -0800

    feat: use go-embed for schema
    Backport the changes in https://github.com/pulumi/pulumi-tf-provider-boilerplate/pull/56 to use go embed instead of generating schema

--- provider/cmd/pulumi-resource-databricks/main.go
+++ provider/cmd/pulumi-resource-databricks/main.go
@@ -12,15 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:generate go run ./generate.go
-
 package main
 
 import (
+	_ "embed"
 	databricks "github.com/paiyar/pulumi-databricks/provider"
 	"github.com/paiyar/pulumi-databricks/provider/pkg/version"
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 )
+
+//go:embed schema.json
+var pulumiSchema []byte
 
 func main() {
 	// Modify the path to point to the new provider
